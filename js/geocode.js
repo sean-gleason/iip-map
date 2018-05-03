c881d34d919e6dda108320120169e80a0ce4f86b
mrewers <marekrewers@gmail.com>
2018-05-03 17:07:46 -0400

    Remove stale code

--- js/geocode.js
+++ js/geocode.js
@@ -7,7 +7,7 @@ var countryField = iip_map_params.screendoor_country;
 
 var googleKey = iip_map_params.google_api_key;
 
-// Make XHR request to Screendoor
+// Make request to Screendoor API
 var request = new XMLHttpRequest();
 request.open('GET', endpoint);
 request.responseType = 'json';
@@ -16,67 +16,18 @@ request.send();
 request.onload = function() {
   var data = request.response;
 
-  printResponse(data);
+  getAddressString(data);
 
 }
 
-function printResponse(jsonObj) {
+// Pull out address info and write to a string
+function getAddressString(jsonObj) {
   jsonObj.forEach(function(item) {
     address = item.responses[cityField] + ', ' + item.responses[countryField];
 
     geocodeAddress(address);
   });
 }
-
-// function geocodeAddress(address) {
-//   var response = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + googleKey;
-//
-//   var request = new XMLHttpRequest();
-//   request.open('GET', response);
-//   request.responseType = 'json';
-//   request.send();
-//
-//   request.onload = function() {
-//     var data = request.response;
-//
-//     newMaker(data);
-//   }
-// }
-// var features = [];
-//
-// function newMaker(data) {
-//
-//   if (data.status == 'OK') {
-//     var latLng = data.results[0].geometry.location;
-//
-//     features.push(latLng);
-//
-//     var marker = new google.maps.Marker({
-//       position: latLng,
-//     });
-//     // console.log(marker.position)
-//   }
-//
-//   // console.log(features);
-// }
-//
-// features.forEach(function(feature) {
-//   var marker = new google.maps.Marker({
-//     position: feature,
-//   });
-//   console.log(marker.position);
-// });
-// Extract city and country for each screendoor entry
-// function initMap() {
-//   var latLng = new google.maps.LatLng(lat, lng)
-//   var mapOptions = {
-//     zoom: zoom,
-//     center: latLng
-//   }
-//
-//   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-//   var geocoder = new google.maps.Geocoder();
-// }
 
 // Geocode event locations to latitude/longitude
 function geocodeAddress(address) {
@@ -89,13 +40,6 @@ function geocodeAddress(address) {
       var lng = results[0].geometry.location.lng();
 
       var latLng = { lat: lat, lng: lng}
-
-      var marker = new google.maps.Marker({
-        map: map,
-        position: latLng,
-      });
-
-      console.log(marker.position);
 
     }
   });
