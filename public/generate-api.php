7dc80f7033b49277c05e97926f67956e8053f2e6
mrewers <marekrewers@gmail.com>
2018-05-30 12:39:45 -0400

    Rewrite API to allow for multiple maps on same site

--- public/generate-api.php
+++ public/generate-api.php
@@ -6,46 +6,73 @@ class IIP_Map_API_Route extends WP_REST_Controller {
     // Define API URL parameters
     $version = '1';
     $namespace = 'iip-map/v' . $version;
-    $base = 'map/';
+    $base = 'maps';
 
+    register_rest_route( $namespace, '/' . $base, array(
+      'methods' => WP_REST_Server::READABLE,
+      'callback' => array($this, 'get_maps'),
+    ) );
+
+    register_rest_route( $namespace, '/' . $base . '/(?P<id>[\d]+)', array(
+      'methods' => WP_REST_Server::READABLE,
+      'callback' => array($this, 'get_map_data'),
+    ) );
+  }
+
+  public function get_maps( $request ) {
     $args = array(
       'post_type' => 'iip_map_data',
       'post_status' => 'publish',
-      'fields' => 'ids'
     );
 
     $maps = get_posts( $args );
 
-    foreach( $maps as $map ) {
-
-      $get_item_args = array('map_id' => $map);
-
-      register_rest_route( $namespace, '/' . $base . $map, array(
-        'methods' => WP_REST_Server::READABLE,
-        'callback' => array($this, 'get_map_data'),
-        'args' => $get_item_args,
-      ) );
+    if ( empty( $maps ) ) {
+      return rest_ensure_response( $data );
     }
+
+    foreach ($maps as $map ) {
+      $data[] = $this->prepare_response_for_collection( $map );
+    }
+
+    return rest_ensure_response($data);
   }
 
   public function get_map_data( $request ) {
-    $item = $get_item_args;
-    $data = array();
+    $id = (int) $request['id'];
+    $response = array();
 
-    $itemdata = $this->prepare_item_for_response( $item, $request );
-    $data[] = $this->prepare_response_for_collection( $itemdata );
+    $map = get_post( $id );
 
-    return new WP_REST_Response( $data, 200 );
+    if ( empty( $map ) ) {
+      return rest_ensure_response( array ());
+    }
+
+    $response = $this->prepare_item_for_response( $map, $request );
+
+    return $response;
   }
 
   public function prepare_item_for_response( $item, $request ) {
+    $id = (int) $request['id'];
+
     global $wpdb;
 
     $table_name = $wpdb->prefix . 'iip_map_data';
 
-    $query = "SELECT map_id, venue_name, venue_address, venue_city, venue_region, venue_country, lat, lng, host_name, event_name, event_desc, event_date, event_time, event_duration, event_topic, contact FROM $table_name";
+    $query = "SELECT map_id, venue_name, venue_address, venue_city, venue_region, venue_country, lat, lng, host_name, event_name, event_desc, event_date, event_time, event_duration, event_topic, contact FROM $table_name WHERE map_id = $id";
     $list = $wpdb->get_results($query);
     return $list;
+  }
+
+  public function prepare_response_for_collection( $response ) {
+    if ( !( $response instanceof WP_REST_Response ) ) {
+      return $response;
+    }
+
+    $data   = (array) $response->get_data();
+
+    return $data;
   }
 
 }
--- public/js/draw-map.js
+++ public/js/draw-map.js
@@ -18,13 +18,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
 let map;
 
+// Load map on page
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: parseFloat(lat), lng: parseFloat(lng)},
     zoom: parseInt(zoom)
   });
 
-  let mapDataEndpoint = '/wp-json/iip-map/v1/map/' + map_id;
+  // Pull map data from iip-maps API
+  let mapDataEndpoint = '/wp-json/iip-map/v1/maps/' + map_id;
   let mapDataXHR = new XMLHttpRequest();
   mapDataXHR.open('GET', mapDataEndpoint);
   mapDataXHR.responseType = 'json';
@@ -40,66 +42,69 @@ function initMap() {
 
 let markers = [];
 
+// Plot markers onto map
 function plotMarkers(m) {
   let infoWin = new google.maps.InfoWindow();
 
+  // Spiderfy to seperate out events in the same location
   let markerSpiderfier = new OverlappingMarkerSpiderfier(map, {
     markersWontMove: true,
     markersWontHide: true,
     basicFormatEvents: true
   });
 
-  m.forEach(function (entry) {
-    entry.forEach(function (item, index) {
-      let latLng = new google.maps.LatLng(parseFloat(item.lat), parseFloat(item.lng))
-      let marker = new google.maps.Marker({
-        position: latLng,
-      });
-
-      if ( item.event_topic !== null ) {
-        topicLine = '<h3 class="iip-map-infowin-header">Topic: ' + item.event_topic + '</h3>';
-      } else {
-        topicLine = '<div></div>';
-      }
-
-      if ( item.host_name !== null ) {
-        hostLine = 'Hosted by: ' + item.host_name + '<br />';
-      } else {
-        hostLine = '';
-      }
-
-      if ( item.contact !== null && item.contact !== '') {
-        contactLine = 'Contact: ' + item.contact;
-      } else {
-        contactLine = '';
-      }
-
-      let windowContent = '<div id="content">'+
-      '<h1 id="firstHeading" class="iip-map-infowin-header">' + item.event_name + ' </h1>' +
-      '<div id="bodyContent" class="iip-map-infowin-body">' +
-      topicLine +
-      '<p>' + item.event_desc + '</p>'+
-      '<h3 class="iip-map-infowin-header">When: </h3>' +
-      '<p> On ' + item.event_date + ' at ' + item.event_time + '. <br />' +
-      'Estimated duration: ' + item.event_duration + '</p>' +
-      '<h3 class="iip-map-infowin-header">Where: </h3>' +
-      '<p>' + hostLine +
-      item.venue_address + '<br />' +
-      item.venue_city + '<br />' +
-      contactLine + '</p>' +
-      '</div>' +
-      '</div>';
-
-      google.maps.event.addListener(marker, 'click', function(evt) {
-        infoWin.setContent(windowContent);
-        infoWin.open(map, marker);
-      });
-
-      markers.push(marker);
-      markerSpiderfier.addMarker(marker);
+  m.forEach(function (item) {
+    let latLng = new google.maps.LatLng(parseFloat(item.lat), parseFloat(item.lng))
+    let marker = new google.maps.Marker({
+      position: latLng,
     });
+
+    // Conditionals for the InfoWindows
+    if ( item.event_topic !== null ) {
+      topicLine = '<h3 class="iip-map-infowin-header">Topic: ' + item.event_topic + '</h3>';
+    } else {
+      topicLine = '<div></div>';
+    }
+
+    if ( item.host_name !== null ) {
+      hostLine = 'Hosted by: ' + item.host_name + '<br />';
+    } else {
+      hostLine = '';
+    }
+
+    if ( item.contact !== null && item.contact !== '') {
+      contactLine = 'Contact: ' + item.contact;
+    } else {
+      contactLine = '';
+    }
+
+    // Text of the InfoWindow
+    let windowContent = '<div id="content">'+
+    '<h1 id="firstHeading" class="iip-map-infowin-header">' + item.event_name + ' </h1>' +
+    '<div id="bodyContent" class="iip-map-infowin-body">' +
+    topicLine +
+    '<p>' + item.event_desc + '</p>'+
+    '<h3 class="iip-map-infowin-header">When: </h3>' +
+    '<p> On ' + item.event_date + ' at ' + item.event_time + '. <br />' +
+    'Estimated duration: ' + item.event_duration + '</p>' +
+    '<h3 class="iip-map-infowin-header">Where: </h3>' +
+    '<p>' + hostLine +
+    item.venue_address + '<br />' +
+    item.venue_city + '<br />' +
+    contactLine + '</p>' +
+    '</div>' +
+    '</div>';
+
+    google.maps.event.addListener(marker, 'click', function(evt) {
+      infoWin.setContent(windowContent);
+      infoWin.open(map, marker);
+    });
+
+    markers.push(marker);
+    markerSpiderfier.addMarker(marker);
   })
 
+  // MarkerClusterer to group adjacent markers
   let markerCluster = new MarkerClusterer(map, markers, {imagePath: '/wp-content/plugins/iip-map/images/m'});
   markerCluster.setMaxZoom(15);
 }
