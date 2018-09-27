513980356821703543da422096f945a75377fb22
mrewers <mrewers@users.noreply.github.com>
2018-09-27 17:13:50 -0400

    Frame out new admin area

---
+++ admin/js/admin-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'React admin page';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('iip-map-admin-page')
+);
+
+module.hot.accept();
