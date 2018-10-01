967690bcf501cc2e952f72bb46a39a98db65e659
mrewers <mrewers@users.noreply.github.com>
2018-10-01 10:28:05 -0400

    Populate metaboxes

--- admin/js/admin-app/src/components/Metaboxes/DataExporter.jsx
+++ admin/js/admin-app/src/components/Metaboxes/DataExporter.jsx
@@ -4,7 +4,14 @@ const DataExporter = ( ) => (
   <div className="postbox">
     <h2 className="iip-map-admin-metabox-header hndle ui-sortable-handle">Export Project Data</h2>
     <div className="inside">
-      Test
+      <p>Click the button below to export the data for this map as a CSV. This dataset includes all venue and event information for each event in this project.</p>
+      <button
+        className="button button-primary button-large"
+        id="iip-map-admin-export-project-data"
+        type="button"
+      >
+        Export Project Data
+      </button>
     </div>
   </div>
 );
--- admin/js/admin-app/src/components/Metaboxes/FormSelector.jsx
+++ admin/js/admin-app/src/components/Metaboxes/FormSelector.jsx
@@ -8,7 +8,8 @@ const FormSelector = ( { formType, projectId, setId } ) => (
   <div className="iip-map-admin-screendoor-project-container" id={ `${formType}-project-selector` }>
     <form className="iip-map-admin-project-form">
       <label className="iip-map-admin-label" htmlFor={ `${formType}-project-id` }>
-        { capitalize( formType ) } Project ID:
+        { capitalize( formType ) }
+        { ' Project ID:' }
         <input
           className="iip-map-admin-project-input"
           id={ `${formType}-project-id` }
