061ee53b2e728727be56a944e0d6a76871cb9723
Braxton Diggs <braxtondiggs@gmail.com>
2019-06-28 13:48:33 -0400

    Fix formatting

--- admin/js/src/components/Metaboxes/DataImporter.jsx
+++ admin/js/src/components/Metaboxes/DataImporter.jsx
@@ -12,13 +12,17 @@ const DataImporter = () => {
   const setActionLoading = () => setAction( { loading: true, message: null } );
   const setActionError = err => setAction( { loading: false, error: true, message: err } );
   const setActionResult = err => setAction( { loading: false, error: false, message: err } );
-  const handleFileSelect = (e) => {
-    const files = e.target.files;
-    if(files && files.length > 0) {
+  const handleFileSelect = ( e ) => {
+    const { files } = e.target;
+    if ( files && files.length > 0 ) {
       setActionLoading();
       importScreenDoorData( files[0] ).then( ( result ) => {
-        // TODO:
-      }).catch( setActionError );
+        if ( result.success ) {
+          setActionResult( 'Import successful.' );
+        } else if ( result.error ) {
+          setActionError( `Import Failed: ${result.error}` );
+        }
+      } ).catch( setActionError );
     }
   };
 
@@ -32,26 +36,28 @@ const DataImporter = () => {
           type="button"
           className="button button-primary button-large"
           id="iip-map-admin-import-screendoor-data"
-          disabled={ action.loading }>
+          disabled={ action.loading }
+        >
           <label
-            for="iip-map-admin-import-screendoor-data-input"
-            style={labelStyle}>
+            htmlFor="iip-map-admin-import-screendoor-data-input"
+            style={ labelStyle }
+          >
             Import Screendoor Data
+            <input
+              id="iip-map-admin-import-screendoor-data-input"
+              className="hidden"
+              accept=".csv,text/csv"
+              type="file"
+              onChange={ handleFileSelect }
+            />
           </label>
-          <input
-            id="iip-map-admin-import-screendoor-data-input"
-            class="hidden"
-            accept=".csv,text/csv,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
-            type="file"
-            onChange={ handleFileSelect } />
         </button>
-
         <div className="iip-map-admin-marker-row">
           { action.message && (
             <div className={ `iip-map-admin-marker-${action.error ? 'error' : 'success'}` }>
               { action.message }
             </div>
-          )}
+          ) }
         </div>
       </div>
     </div>
